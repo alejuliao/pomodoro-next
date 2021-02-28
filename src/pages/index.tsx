c03c435672f19806593dedb4f60c09cf8c17c7c5
alejuliao <alexand.juliao@gmail.com>
2021-02-28 03:17:26 -0300

    pomodoro-nextjs

---
+++ src/pages/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ExperienceBar } from '../components/ExperienceBar';
+import { Profile } from '../components/Profile';
+import styles from '../styles/components/Home.module.css';
+
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+
+    <ExperienceBar/>
+
+    <section>
+      <div>
+        <Profile/>
+      </div>
+      <div>
+
+      </div>
+
+    </section>
+  </div>
+  )
+}
