bdee9ebac0d9b27ee80dcdf74e04a88c808620e2
alejuliao <alexand.juliao@gmail.com>
2021-02-28 12:42:29 -0300

    end of the Second Day - Time -  start Countdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,27 @@
+import Head from 'next/head';
 import React from 'react';
+import { CompleteChallenges } from '../components/CompleteChallenges';
+import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
 import styles from '../styles/components/Home.module.css';
 
 
+
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | myPomo</title>
+      </Head>
 
     <ExperienceBar/>
 
     <section>
       <div>
         <Profile/>
+        <CompleteChallenges/>
+        <Countdown/>
       </div>
       <div>
 
